408a93a6217593611a2a892d19786a5e9dfa7b35
Aditya <adi97rao@gmail.com>
2023-01-03 22:23:50 +0530

    AddTwoNumbers

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,19 @@
+public class Add_Two_Numbers {
+        public static void twoSum(int[] nums, int target) {
+            int[] arr = new int[2];
+            for(int i = 0;i<nums.length;i++){
+                for(int j=i+1;j<nums.length;j++)
+                if (nums[i]+nums[j]==target){
+                    arr[0] = i;
+                    arr[1] = j;
+                    System.out.println(arr[0] + ", " + arr[1]);
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] nums = new int[]{2,7,11,15};
+            int target = 9;
+            twoSum(nums, target);
+        }
+}
