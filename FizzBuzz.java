9b8deb6e1be8eae4b240867f2ffb5cc9613b5000
Aditya <adi97rao@gmail.com>
2023-08-07 20:13:39 +0530

    fizzBuzzOptimized.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -8,10 +8,10 @@ public class FizzBuzz {
                 if(i%3==0 && i%5==0){
                     resultList.add("FizzBuzz");
                 }
-                else if(i%3==0 && i%5!=0){
+                else if(i%3==0){
                     resultList.add("Fizz");
                 }
-                else if(i%5==0 && i%3!=0){
+                else if(i%5==0){
                     resultList.add("Buzz");
                 }
                 else{
