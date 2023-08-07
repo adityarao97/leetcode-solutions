64f101527b2c74051c237fd93c7c53cd094bb685
Aditya <adi97rao@gmail.com>
2023-08-08 00:33:46 +0530

    palindrome optimized

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,14 @@
 public class Palindrome {
     public static boolean isPalindrome(int x) {
-            String str = (x + "");
-            String palindrome = "";
-            for(int i = str.length()-1; i >= 0; i--){
-                palindrome = palindrome + str.charAt(i);
-            }
-            if(palindrome.equals(str)){
-                return true;
-            }
+        if((x%10==0 &&x!=0) || x<0){
             return false;
+        }
+        int reverseNum = 0;
+        while(x>reverseNum){
+            reverseNum = reverseNum*10 + x%10;
+            x=x/10;
+        }
+        return x==reverseNum || x==(reverseNum/10);
         }
 
     public static void main(String[] args) {
