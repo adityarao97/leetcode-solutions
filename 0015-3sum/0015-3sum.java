bc7882ce7a2dabe59d674ce652506feb4e1eda9a
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2024-09-07 09:13:18 -0700

    Time: 29 ms (71.63%), Space: 51.4 MB (58.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,25 +3,25 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         int left = 0, right = 0, sum = 0;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-        //array is sorted hence any combination with same starting value will result in duplicate, so we skip that
+        for(int i = 0; i < nums.length; i++){
+        //since the array is sorted we skip those combinations which start with same values to avoid duplicates    
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            //two pointer approach
             left = i + 1;
             right = nums.length - 1;
-            while(left<right){
+            while(left < right){
                 sum = nums[i] + nums[left] + nums[right];
                 if(sum>0){
                     right--;
-                }
+                } 
                 else if(sum<0){
                     left++;
                 }
                 else{
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
+                    //we skip the 2nd element i.e left if it is same in succession to avoid duplicates
                     while(nums[left] == nums[left-1] && left<right){
                         left++;
                     }
