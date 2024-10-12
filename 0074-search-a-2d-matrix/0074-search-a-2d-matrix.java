12b679e190a38b5dd498a24ce3d711407b20c90d
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2024-10-12 00:53:49 -0700

    Time: 0 ms (100%), Space: 42.3 MB (19.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -11,17 +11,17 @@ class Solution {
     public boolean binarySearch(int[] arr, int target){
         int left = 0, right = arr.length - 1;
         while(left <= right){
-                int mid = (left + right)/2;
-                if(target == arr[mid]){
-                    return true;
-                }
-                else if(target > arr[mid]){
-                    left = mid + 1;
-                }
-                else{
-                    right = mid - 1;
-                }
+            int mid = (left + right)/2;
+            if(target == arr[mid]){
+                return true;
             }
+            else if(target > arr[mid]){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
         return false;
     }
 }
