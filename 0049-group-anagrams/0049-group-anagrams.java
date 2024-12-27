ca0e0d0e7975fbaae451b599ba6a9f71f9d04843
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2024-12-27 11:39:19 -0800

    Time: 7 ms (63.98%), Space: 47.8 MB (56.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,34 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i = 0; i<strs.length; i++){
-            if(set.contains(i)){
-                continue;
-            }
-            List<String> tempArr = new ArrayList<>();
-            tempArr.add(strs[i]);
-            for(int j = i+1; j<strs.length; j++){
-                if(isAnagram(strs[i], strs[j])){
-                    tempArr.add(strs[j]);
-                    set.add(j);
-                }
-            }
-            res.add(tempArr);
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str: strs){
+            char[] chArr = str.toCharArray();
+            Arrays.sort(chArr);
+            String sortedString = new String(chArr);
+            List<String> val = map.getOrDefault(sortedString, new ArrayList<>());
+            val.add(str);
+            map.put(sortedString, val);
         }
-        return res;
-    }
-
-    boolean isAnagram(String s, String t){
-        if(s.length()!=t.length()){
-            return false;
+        List<List<String>> resultArr = new ArrayList<>();
+        for(String key: map.keySet()){
+            resultArr.add(map.get(key));
         }
-        char[] c1 = s.toCharArray();
-        char[] c2 = t.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        s = new String(c1);
-        t = new String(c2);
-        return s.equals(t);
+        return resultArr;
     }
 }
