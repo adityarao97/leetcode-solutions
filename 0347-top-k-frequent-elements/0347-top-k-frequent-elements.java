fdd3aa97e7c49fc5db24a6833ce2a325afe1e34c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-01-01 23:14:19 -0800

    Time: 11 ms (91.7%), Space: 50.2 MB (5.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //put elements in a frequency map -> map
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int size = nums.length; 
-        for(int i=0; i < size; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        //create a frequency map of each value in array key: element, value: frequency of element
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
         }
-        //initialize a list with size n from 0 to n
-        List<Integer>[] list = new ArrayList[size + 1];
-        //fill outer list with indexes from 0->nums.length
-        for(int i=0;i<size+1;i++){
-            list[i] = new ArrayList<>();
+        int size = nums.length+1;
+        //frequency array is a list of array where each array index will contain the corresponding list of items from nums based on their frequency, eg: nums[] = [1,1,1,2,2,3,4,4,4], frequency[1] = <3>, frequency[2] = <2>, frequency[3] = <1,4>
+        List<Integer>[] frequency = new ArrayList[size];
+        for(int i=0; i<size;i++){
+            frequency[i] = new ArrayList<>();
         }
-        //loop across map and based on value insert in list as 
-            //list.add<map.getValue(i+1), list.get(i).add(map.getKey(i))>
         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            int frequency = entry.getValue();
-            list[frequency].add(entry.getKey());
+            frequency[entry.getValue()].add(entry.getKey());
         }
-        //initialize an array with size as k
         int[] res = new int[k];
         int count = 0;
-        //loop through list in reverse order by decreasing k until it is 0 and add in result array
-        for(int i = list.length - 1; i>= 0 ; i--){
-            List<Integer> freq = list[i];
-            for(int num: freq){
-                res[count++] = num;
+        int frequencySize = frequency.length - 1;
+        //the top occuring k elements can be read in reverse order of the frequency array as the highest frequency element will be stored at the last index 
+        for(int i = frequencySize; i>=0; i--){
+            List<Integer> temp = frequency[i];
+            for(int j: temp){
+                res[count++] = j;
                 if(count==k){
                     return res;
                 }
             }
         }
-        return new int[0];
+        return res;
     }
 }
