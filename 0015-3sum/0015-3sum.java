be7311c6160381d3680e23823b6baa97bf0d2933
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-01-03 22:58:12 -0800

    Time: 32 ms (43.61%), Space: 51.3 MB (85.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        int left = 0, right = 0, sum = 0;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-        //since the array is sorted we skip those combinations which start with same values to avoid duplicates    
+        int left = 0, right = 0, sum=0, len = nums.length;
+        for(int i=0; i<len;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            left = i + 1;
-            right = nums.length - 1;
-            //two pointer approach
-            while(left < right){
+            left = i+1;
+            right = len-1;
+            while(left<right){
                 sum = nums[i] + nums[left] + nums[right];
-                if(sum>0){
-                    right--;
-                } 
-                else if(sum<0){
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
-                }
-                else{
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    //we skip the 2nd element i.e left if it is same in succession to avoid duplicates
-                    while(nums[left] == nums[left-1] && left<right){
+                    while(left<right && nums[left]==nums[left-1]){
                         left++;
                     }
                 }
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
             }
-        }
-        return result;
+        } 
+        return res;
     }
 }
