18a9fd7b3b35649298c09634fa589ffcdb32f92c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-03 19:15:26 -0700

    Time: 1 ms (100%), Space: 42.5 MB (61.9%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,74 +1,48 @@
 class Solution {
-
-    private static final int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
-    private int bfs(int[][] grid, List<int[]> list,int[][] visited){
-        Queue<int[]> queue = new LinkedList<>();
-        for(int[] it: list){
-            queue.offer(it);
-        }
-        int time = 0;
-        while(!queue.isEmpty()){
-            time =  queue.peek()[2];
-            for(int i = 0; i < queue.size(); i++){                
-                int[] node = queue.poll();
-                int row = node[0];
-                int col = node[1];
-                time++;
-                for(int[] direction : directions){
-                    if(row + direction[0] >= 0 && row + direction[0] < grid.length && col + direction[1] >= 0 && col + direction[1] < grid[0].length){
-                        if(visited[row + direction[0]][col + direction[1]] == 0 && grid[row + direction[0]][col + direction[1]] == 1){
-                            grid[row + direction[0]][col + direction[1]] = 2;
-                            queue.offer(new int[]{row + direction[0], col + direction[1], time});
-                            visited[row + direction[0]][col + direction[1]] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                if(grid[row][col] == 1){
-                    return -1;
-                }
-            }
-        }
-        return time;
-    }
-
     public int orangesRotting(int[][] grid) {
+        //define a queue which will contain an array of integer each storing grid position where oranges are rotten 
         Queue<int[]> queue = new LinkedList<>();
+        //define a fresh pointer to track total number of fresh oranges
         int fresh = 0;
+        // define a time pointer to identity total time taken
         int time = 0;
-        for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                if(grid[row][col] == 2){
-                    queue.offer(new int[]{row, col});
-                }
-                if(grid[row][col] == 1){
+        //loop through the grid, if an orange is rotten push it onto the queue and if it is fresh increase counter for fresh
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int row=0; row<m;row++){
+            for(int col=0;col<n;col++){
+                int cell = grid[row][col];
+                if(cell==1){
                     fresh++;
                 }
+                if(cell==2){
+                    queue.offer(new int[]{row, col});
+                }
             }
         }
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        while(!queue.isEmpty() && fresh>0){
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        //until no fresh oranges are left or until queue is empty do bfs where oranges are rotten
+        while(fresh>0 && !queue.isEmpty()){
             int length = queue.size();
-            for(int i=0; i < length; i++){
-                int[] node = queue.poll();
-                int row = node[0];
-                int col = node[1];
+            for(int i=0; i<length; i++){
+                int[] cell = queue.poll();
+                int row = cell[0];
+                int col = cell[1]; 
+                //loop through each direction to get adjacent cells if they are fresh make them rotten and add it to queue
                 for(int[] direction: directions){
-                    int nextRowCell = row + direction[0];
-                    int nextColCell = col + direction[1];
-                    if(nextRowCell >= 0 && nextRowCell < grid.length && nextColCell >= 0 && nextColCell < grid[0].length && grid[nextRowCell][nextColCell] == 1){
-                        grid[nextRowCell][nextColCell] = 2;
-                        queue.offer(new int[]{nextRowCell, nextColCell});
+                    int r = row + direction[0];
+                    int c = col + direction[1];
+                    if(r>=0 && r<m && c>=0 && c<n && grid[r][c]==1){
+                        grid[r][c] = 2;
                         fresh--;
+                        queue.offer(new int[]{r,c});
                     }
                 }
             }
+            //increase time pointer after each cycle
             time++;
         }
-        return fresh!=0 ? -1 : time;
+        //if fresh pointer is not zero return -1 else return time value
+        return fresh>0 ? -1 : time;
     }
 }
