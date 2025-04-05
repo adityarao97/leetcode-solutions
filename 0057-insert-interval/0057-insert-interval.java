c04d2d7c4c2d990c79f2c9b8ee5bcda8ee68ebd4
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-04 20:02:53 -0700

    Time: 1 ms (99.05%), Space: 44.8 MB (77.11%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -2,16 +2,22 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> list = new ArrayList<>();
         int n = intervals.length, i = 0;
+        //Loop until the end element of interval is less than the first element of newInterval
         while(i<n && intervals[i][1] < newInterval[0]){
+            //keep adding the intervals to list
             list.add(intervals[i]);
             i++;
         }
+        //now we can merge the newInterval to intervals
+        //check until when is the end of newInterval greater than the start of interval item, update newInterval based on the min and max value
         while(i<n && newInterval[1]>=intervals[i][0]){
             newInterval[0]=Math.min(intervals[i][0], newInterval[0]);
             newInterval[1]=Math.max(intervals[i][1], newInterval[1]);
             i++;
         }
+        //add it to list
         list.add(newInterval);
+        //add remaining intervals
         while(i<n){
             list.add(intervals[i]);
             i++;
