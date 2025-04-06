eebb00ffecf1d2658e1d56354c94090ad35e78fd
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-05 17:25:23 -0700

    Time: 0 ms (100%), Space: 40.8 MB (95.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,32 @@
 class Solution {
 
     private int maxSub(int index, int[] nums, List<Integer> dp){
+        //base case if index ==0 return nums[0] if <0 return 0
         if(index == 0){
-            return nums[index];
+            return nums[0];
         }
         if(index < 0){
             return 0;
         }
+        //if value exists in dp return it
         if(dp.get(index)!=-1) return dp.get(index);
+        //at each step pick the current item in array or don't
+        //if we pick we will call the function with next alternate value hence index - 2 
         int pick = nums[index] + maxSub(index - 2, nums, dp);
-        int notPick = 0 + maxSub(index - 1, nums, dp);
-        dp.set(index, Math.max(pick, notPick)); 
+        //if we don't pick we will call the next element in arrary with index - 1
+        int notPick = maxSub(index - 1, nums, dp);
+        //we take the maximum of the two decision
+        dp.set(index, Math.max(pick, notPick));
         return dp.get(index);
-    } 
+    }
 
     public int rob(int[] nums) {
+        //initialize a dp list with all values = -1
         List<Integer> dp = new ArrayList<>();
         for(int n: nums){
             dp.add(-1);
         }
+        //call from last index a recursive function
         return maxSub(nums.length - 1, nums, dp);
     }
 }
