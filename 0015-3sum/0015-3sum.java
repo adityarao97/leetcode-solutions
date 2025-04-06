02d75fce242b096b889f05be6d48d4703e7f37e2
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-06 14:23:45 -0700

    Time: 33 ms (37.07%), Space: 51.3 MB (92.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        //sort the array 0(nlogn)
         Arrays.sort(nums);
-        int left = 0, right = 0, sum=0, len = nums.length;
-        for(int i=0; i<len;i++){
-            if(i>0 && nums[i]==nums[i-1]){
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        for(int i=0; i < n; i++){
+            //if adjacent elements are same it would lead to duplicate triplets hence we skip over them
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-            left = i+1;
-            right = len-1;
-            while(left<right){
-                sum = nums[i] + nums[left] + nums[right];
-                if(sum==0){
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+            left = i + 1;
+            right = n - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
-                    while(left<right && nums[left]==nums[left-1]){
+                    //we'd want to check again if we encounter same values at left index to ignore duplicates
+                    while(left < right && nums[left] == nums[left - 1]){
                         left++;
                     }
                 }
-                else if(sum>0){
-                    right--;
-                }
-                else{
-                    left++;
-                }
+                //if sum is more than zero we'd want to reduce the sum by decreasing right pointer
+                else if(sum > 0) right--;
+                //if sum is less than 0 we'd want to increase the sum by increasing the left pointer
+                else left++;
             }
-        } 
-        return res;
+        }
+        return result;
     }
 }
