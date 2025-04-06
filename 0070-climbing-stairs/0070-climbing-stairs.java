f0b05023e709a1106f61e2084eb6700b8067dc4c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-05 18:32:08 -0700

    Time: 0 ms (100%), Space: 40.6 MB (38.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public int numberOfWays(int idx, int[] dp){
+        //at 0th index only 1 step is possible
         if(idx == 0) return 1;
         if(idx<0) return 0;
         if(dp[idx]!=-1) return dp[idx];
+        //at each step either take 2 step or two steps
         return dp[idx] = numberOfWays(idx - 2, dp) + numberOfWays(idx - 1, dp);
     }
 
@@ -12,6 +14,7 @@ class Solution {
         for(int i=0;i<=n;i++){
             dp[i] = -1;
         }
+        //top down approach
         return numberOfWays(n, dp);
     }
 }
