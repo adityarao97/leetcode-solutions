654833ee2f49a7c3f6f82ec630c6f9d1e8712f81
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-05 20:54:47 -0700

    Time: 96 ms (42.19%), Space: 55.6 MB (85.75%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -1,14 +1,18 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
         if(k==1) return 0;
+        //the first and last index will always be same irrespective of input so on maxScore - minScore they will always be 0 hence we can ignore them
         int n = weights.length;
         List<Integer> pairSums = new ArrayList<>();
+        //we store pairSums which is a sum of elements on border indexes on all possible splits
         for(int i=0; i<n-1; i++){
             pairSums.add(weights[i] + weights[i+1]);
         }
+        //sort them 
         Collections.sort(pairSums);
         n = pairSums.size();
         long minScore = 0, maxScore = 0;
+        //min score will be on first k splits and max score will be on last k splits
         for(int i=0; i<k-1; i++){
             minScore += pairSums.get(i);
             maxScore += pairSums.get(n - 1 - i);
