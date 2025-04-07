ca8831f85cbd1e8edf555d9f8fa6d807b63a452f
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-06 23:57:08 -0700

    Time: 0 ms (100%), Space: 41.9 MB (82.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,12 +6,12 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[left] <= nums[mid]){
-                if(target>=nums[left] && target<nums[mid]){
+            else if(nums[left] <= nums[mid]){
+                if(target>=nums[left] && target<=nums[mid]){
                     right = mid - 1;
                 }
                 else{
-                    left = mid + 1; 
+                    left = mid + 1;
                 }
             }
             else{
@@ -19,7 +19,7 @@ class Solution {
                     left = mid + 1;
                 }
                 else{
-                    right = mid - 1; 
+                    right = mid - 1;
                 }
             }
         }
