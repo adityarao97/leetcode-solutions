f7cf81942fc86083fb352c6e0acce96ec79a0adf
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-06 23:36:12 -0700

    Time: 27 ms (22.97%), Space: 45.2 MB (59.88%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 0, right = Arrays.stream(piles).max().getAsInt(), speed = 0, ans = 0, time = 0;
-        while(left<=right){
+        int left = 0, right = Arrays.stream(piles).max().getAsInt();
+        int ans = 0, speed = 0, time = 0;
+        while(left <= right){
             speed = (left + right)/2;
             time = calculateTime(piles, speed);
-            if(time <= h){
+            if(time<=h){
                 ans = speed;
-                right = speed - 1; 
+                right = speed - 1;
             }
             else{
                 left = speed + 1;
@@ -15,10 +16,10 @@ class Solution {
         return ans;
     }
 
-    public int calculateTime(int[] piles, int speed){
+    private int calculateTime(int[] piles, int mid){
         int time = 0;
         for(int pile: piles){
-            time += Math.ceil((double) pile/speed);
+            time+=Math.ceil((double)pile/mid);
         }
         return time;
     }
