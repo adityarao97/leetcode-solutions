a0c8048d0bceb72abf8a7907a48a800b76a7deb5
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-04-07 23:25:47 -0700

    Time: 1 ms (99.9%), Space: 45.6 MB (70.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,13 @@
 class Solution {
 
     public boolean canJump(int[] nums) {
+        //goalpost is initially at the last index
         int goalPost = nums.length - 1;
+        //we keep shifting goal post by checking if it is reachable from its previous index
         for(int i = nums.length - 2; i>=0; i--){
             if(i + nums[i] >= goalPost) goalPost = i;
         }
+        //return true if goalpost is shifted to 0th index
         return (goalPost == 0);
     }
 }
