d3aa2f7e66aec37fe87907166d11dab7e03b8e87
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-06-01 09:24:52 -0700

    Time: 2 ms (83.7%), Space: 44.8 MB (46.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,22 +2,22 @@ class Solution {
 
     List<List<Integer>> result;
 
-    public void backtrack(int[] candidates, int target, List<Integer> currentList, int index){
+    public void dfs(int index, List<Integer> currentList, int target, int[] candidates){
         if(target == 0){
             result.add(new ArrayList(currentList));
             return;
         }
         if(target < 0 || index >= candidates.length) return;
         currentList.add(candidates[index]);
-        backtrack(candidates, target - candidates[index], currentList, index);
+        dfs(index, currentList, target-candidates[index], candidates);
         currentList.remove(currentList.size()-1);
-        backtrack(candidates, target, currentList, index + 1);
+        dfs(index + 1, currentList, target, candidates);
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target){
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         result = new ArrayList<>();
         List<Integer> currentList = new ArrayList<>();
-        backtrack(candidates, target, currentList, 0);
+        dfs(0, currentList, target, candidates);
         return result;
     }
 }
