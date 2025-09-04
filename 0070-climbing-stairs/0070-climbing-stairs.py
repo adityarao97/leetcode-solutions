75cf48a0babf8653b3c9552ef2b653ff6c21d2e2
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-09-04 08:37:43 -0700

    Time: 0 ms (100%), Space: 12.4 MB (54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,31 @@
 class Solution(object):
     def climbStairs(self, n):
-        dp = [-1] * (n+1)        
-        def recursive(idx, dp):
-            if idx==0: return 1
-            if idx<0: return 0
-            if dp[idx]!=-1: return dp[idx]
-            dp[idx] = recursive(idx-1, dp) + recursive(idx-2, dp)
-            return dp[idx]
-        return recursive(n, dp)
+        
+        # Space Optimized
+        if n<=2: return n
+        prev2, prev = 1, 2
+        for i in range(2, n):
+            cur = prev + prev2
+            prev2 = prev
+            prev = cur
+        return prev
+
+        # Tabulation
+        # if n<=2: return n
+        # dp = [0] * (n)
+        # dp[0], dp[1] = 1, 2
+        # for i in range(2, n):
+        #     dp[i] = dp[i-1]+dp[i-2]
+        # return dp[n-1]
+
+        # Recursive
+        # dp = [-1] * (n+1)        
+        # def recursive(idx, dp):
+        #     if idx==0: return 1
+        #     if idx<0: return 0
+        #     if dp[idx]!=-1: return dp[idx]
+        #     dp[idx] = recursive(idx-1, dp) + recursive(idx-2, dp)
+        #     return dp[idx]
+        # return recursive(n, dp)
         
         
