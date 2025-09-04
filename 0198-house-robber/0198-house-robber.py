08128edc17182c7bdc97d5100b3132dca1a1e238
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-09-04 08:16:26 -0700

    Time: 0 ms (100%), Space: 12.4 MB (55.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,33 @@
 class Solution(object):
     def rob(self, nums):
-        dp = [-1] * len(nums)
-        def recursiveMax(idx, nums, dp):
-            if idx < 0: return 0
-            if idx == 0: return nums[idx]
-            if dp[idx]!=-1: return dp[idx]
-            pick = nums[idx] + recursiveMax(idx-2, nums, dp)
-            notPick = recursiveMax(idx-1, nums, dp)
-            dp[idx] = max(pick,notPick)
-            return dp[idx]
-        return recursiveMax(len(nums)-1, nums, dp)
+        #space optimization
+        prev = nums[0]
+        prev2 = 0
+        for i in range(1, len(nums)):
+            pick = nums[i]
+            if(i>1): pick+=prev2
+            notPick = prev
+            cur = max(pick, notPick)
+            prev2 = prev
+            prev = cur
+        return prev
+        #tabulation + memoization
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
+        # for i in range(1, len(nums)):
+        #     pick = nums[i]
+        #     if i>1: pick+=dp[i-2]
+        #     notPick = dp[i-1]
+        #     dp[i] = max(pick, notPick)
+        # return dp[-1]
+        #recursive solution
+        # def recursiveMax(idx, nums, dp):
+        #     if idx < 0: return 0
+        #     if idx == 0: return nums[idx]
+        #     if dp[idx]!=-1: return dp[idx]
+        #     pick = nums[idx] + recursiveMax(idx-2, nums, dp)
+        #     notPick = recursiveMax(idx-1, nums, dp)
+        #     dp[idx] = max(pick,notPick)
+        #     return dp[idx]
+        # return recursiveMax(len(nums)-1, nums, dp)
         
