7104d14b7db2087a9f1e020dee47f00ada225fd6
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-09-08 18:35:08 -0700

    Time: 15 ms (85.24%), Space: 42.1 MB (87.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,33 +1,23 @@
 class Solution {
-    
+
+    public void checkPalindrome(String s, int l, int r, int[] resIdx, int[] resLen){
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+                if(r-l+1 > resLen[0]){
+                    resLen[0] = r - l + 1;
+                    resIdx[0] = l;
+                }
+                l--;
+                r++;
+            }
+    }
+
     public String longestPalindrome(String s) {
-        int resLen = 0;
-        int resIdx = 0;
-        int l = 0;
-        int r = 0;
-        for(int i=0;i<s.length();i++){
-            //Odd: single center element and keep checking for outer elements
-            l = r = i;
-            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
-                if(r - l + 1 > resLen){
-                    resLen = r - l + 1;
-                    resIdx = l;
-                }
-                l--;
-                r++;
-            }
-            //Even: for two elements keep checking for outer elements 
-            l = i;
-            r = i + 1;
-            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
-                if(r - l + 1 > resLen){
-                    resLen = r - l + 1;
-                    resIdx = l;
-                }
-                l--;
-                r++;
-            }
+        int[] resIdx = new int[1];
+        int[] resLen = new int[1];
+        for(int i=0; i < s.length(); i++){
+            checkPalindrome(s, i, i, resIdx, resLen);
+            checkPalindrome(s, i, i+1, resIdx, resLen);
         }
-        return s.substring(resIdx, resIdx + resLen);
+        return s.substring(resIdx[0], resIdx[0] + resLen[0]);
     }
 }
