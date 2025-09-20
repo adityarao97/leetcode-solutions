07ab054bbc88be8019d29863d7f7fa5b9faf5640
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-09-20 16:52:00 -0700

    Time: 5 ms (9.88%), Space: 19.5 MB (89.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,11 +3,14 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
+            #if new interval is strictly before current interval append to list and return remaining items
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
+            #if new interval is strictly after current interval append current interval
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
+            #if the intervals are overlapping merge them and update the newInterval
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
 
