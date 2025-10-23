e3603f69348700e38d20714d1da3645dcf894fe9
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-10-22 19:07:50 -0700

    Time: 0 ms (100%), Space: 18.7 MB (13.7%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,23 +1,33 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        adj = [[] for _ in range(n+1)]
-
-        def dfs(node, parent):
-            if visited[node]:
-                return True
-            visited[node] = True
-            for neighbor in adj[node]:
-                if neighbor == parent:
-                    continue
-                if dfs(neighbor, node):
-                    return True
-            return False
-        
+        adj = [[] for _ in range(n + 1)]
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-            visited = [False] * (n+1)
-            if dfs(u, -1):
+        visited = [False] * (n + 1)
+        cyclestart = -1
+        cycle = set()
+
+        def dfs(node, parent):
+            nonlocal cyclestart
+            if visited[node]:
+                cyclestart = node
+                return True
+            visited[node] = True
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if dfs(nei, node):
+                    if cyclestart != -1:
+                        cycle.add(node)
+                    if cyclestart == node:
+                        cyclestart = -1
+                    return True
+            return False
+        
+        dfs(1, -1)
+        for u, v in reversed(edges):
+            if u in cycle and v in cycle:
                 return [u, v]
         return []
