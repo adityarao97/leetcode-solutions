a4d0d514f2034dbcad579408d238be6b4f5aad4b
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-10-25 17:41:31 -0700

    Time: 29 ms (55.72%), Space: 18.6 MB (25.92%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,20 +1,21 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        #visited set with 1st cell marked
+        #initialise a min heap with height of 1st cell, r and c
+        minHeap = [[grid[0][0], 0, 0]]
+        #visited set to keep track of visited cells initialized with 1st cell
         visited = set((0, 0))
-        #min heap with - maxheight in path, r, c initialized with 1st cell
-        minH = [[grid[0][0], 0, 0]]
+        #directions array for traversal across each direction
         directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-        while minH:
-            t, r, c = heapq.heappop(minH)
-            #base case to return the t value which will contain max height encountered and when last cell is reached 
-            if r == N-1 and c == N-1:
+        while minHeap:
+            t, r, c = heapq.heappop(minHeap)
+            #base case will be when we reach the last cell and return the time t
+            if r == N - 1 and c == N - 1:
                 return t
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
                 if nr < 0 or nc < 0 or nr == N or nc == N or (nr, nc) in visited:
                     continue
                 visited.add((nr, nc))
-                #we take the max height value of visited neighbour after each pop from minheap
-                heapq.heappush(minH,[max(t, grid[nr][nc]), nr, nc])
+                #we only want the max height in the current traversal so we compare from the popped height of parent cell and current height and take the max value  
+                heapq.heappush(minHeap, [max(t, grid[nr][nc]), nr, nc])
