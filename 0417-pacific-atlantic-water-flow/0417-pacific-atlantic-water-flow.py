21fe714f62b72eddd49930ff6b0f81d50847d6c9
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-10-27 18:57:30 -0700

    Time: 65 ms (12.26%), Space: 19.5 MB (38.36%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,28 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pacific, atlantic = set(), set()
-        res = []
+        #two sets containing coordinates of cells which can go to pacific and atlantic oceans
+        pac, atl = set(), set()
         directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-
         def dfs(r, c, visited, prevHeight):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visited or heights[r][c] < prevHeight:
+            if r not in range(ROWS) or c not in range(COLS) or (r, c) in visited or heights[r][c] < prevHeight:
                 return
             visited.add((r, c))
-            for direction in directions:
-                dfs(r + direction[0], c + direction[1], visited, heights[r][c])
-
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                dfs(nr, nc, visited, heights[r][c])
         for r in range(ROWS):
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
-
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         for c in range(COLS):
-            dfs(0, c, pacific, heights[0][c])
-            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
-
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pacific and (r, c) in atlantic:
+                if (r, c) in pac and (r, c) in atl:
                     res.append([r, c])
-
         return res
