388ae4d8220bf10145632eda12a1c954be6c7db5
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-03 19:02:08 -0800

    Time: 4 ms (31.47%), Space: 18.9 MB (7.02%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,33 +1,39 @@
+#intuition : run a dfs with first node, try to find the cycle, note where the cycle starts and start adding it to cycle set in reverse, then reverse the edges list and check which node exists in the cycle set and return it
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        adj = [[] for _ in range(n + 1)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-        visited = [False] * (n + 1)
-        cyclestart = -1
+        #create adjacency list of n + 1 edges as we want to include the n'th node
+        adj = { i : [] for i in range(n + 1) }
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+        visited = set()
+        #cycle set will include all nodes encountered in the cycle
         cycle = set()
-
+        cyclestart = -1
         def dfs(node, parent):
+            #nonlocal keyword is used to pick variable value from out of the function
             nonlocal cyclestart
-            if visited[node]:
+            #if we encounter the same node we've detected a cycle and we return true
+            if node in visited:
                 cyclestart = node
                 return True
-            visited[node] = True
+            visited.add(node)
+            #check recursively for neighbouring nodes
             for nei in adj[node]:
-                if nei == parent:
-                    continue
-                if dfs(nei, node):
-                    if cyclestart != -1:
-                        cycle.add(node)
-                    if cyclestart == node:
-                        cyclestart = -1
-                    return True
+                if nei != parent:
+                    if dfs(nei, node):
+                        #dfs is true this element is part of cycle so we add it to the cycle node 
+                        if cyclestart != -1:
+                            cycle.add(node)
+                        #we've reached the first node in the cycle so we set it to -1
+                        if cyclestart == node:
+                            cyclestart = -1
+                        return True
             return False
         
         dfs(1, -1)
-        for u, v in reversed(edges):
-            if u in cycle and v in cycle:
-                return [u, v]
+        for n1, n2 in reversed(edges):
+            if n1 in cycle and n2 in cycle:
+                return [n1, n2]
         return []
