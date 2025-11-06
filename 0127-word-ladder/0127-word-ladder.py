0100600ffb96963ab150f3ce3aa4911dc832c367
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-05 17:46:20 -0800

    Time: 52 ms (87.27%), Space: 20.9 MB (38.29%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,28 +1,27 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        #intuition : turn all words into a wild card ex: dog -> *og, d*g, do* -> then map them to the list of all words in wordlist
+        #do a bfs traversal using a queue and visited set to start from begin word and try to reach end word -> you'll get the min path
         if endWord not in wordList:
             return 0
-        #it'll contain a map of pattern and list of all words that match the pattern
-        nei = collections.defaultdict(list)
-        #create adj. list for all possible patterns of character, * is used to identify the wild card
+        wordPattern = defaultdict(list)
         for word in wordList:
             for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1:]
-                nei[pattern].append(word)
-        #do a bfs traversal to find the shortest path which is the result
-        visited = set([beginWord])
+                pattern = word[:j] + "*" + word[j+1:]
+                wordPattern[pattern].append(word)
         q = deque([beginWord])
-        res = 1
+        visited = set([beginWord])
+        result = 1
         while q:
             for _ in range(len(q)):
                 word = q.popleft()
                 if word == endWord:
-                    return res
+                    return result
                 for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j + 1:]
-                    for neighbour in nei[pattern]:
-                        if neighbour not in visited:
-                            visited.add(neighbour)
-                            q.append(neighbour)
-            res += 1
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for nextWord in wordPattern[pattern]:
+                        if nextWord not in visited:
+                            visited.add(nextWord)
+                            q.append(nextWord)
+            result += 1
         return 0
