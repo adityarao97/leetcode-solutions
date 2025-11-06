fb6aca9e0e7ff83c731fc3f5eda6ae5b955706d0
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-05 18:30:58 -0800

    Time: 264 ms (5.03%), Space: 25.6 MB (12.52%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,35 +1,21 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        #we use dfs to traverse the grid wherever the water is present i.e grid[r][c] == 1 and if the dfs traversal goes out of bounds add 1 to parameter as only then the parameter increases, if it is surrounded by water we return 0
         ROWS, COLS = len(grid), len(grid[0])
-        perimeter = 0
         visited = set()
         directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         def dfs(r, c):
-            if r not in range(ROWS) or c not in range(COLS) or (r, c) in visited or grid[r][c] == 0:
-                return
+            if r not in range(ROWS) or c not in range(COLS) or grid[r][c] == 0:
+                return 1
+            if (r, c) in visited:
+                return 0
             visited.add((r, c))
-            nonlocal perimeter
-            if r == 0:
-                perimeter += 1
-            if r == ROWS - 1:
-                perimeter += 1
-            if c == 0:
-                perimeter += 1
-            if c == COLS - 1:
-                perimeter += 1
-            if r - 1 in range(ROWS) and grid[r - 1][c] == 0:
-                perimeter += 1
-            if r + 1 in range(ROWS) and grid[r + 1][c] == 0:
-                perimeter += 1
-            if c - 1 in range(COLS) and grid[r][c - 1] == 0:
-                perimeter += 1
-            if c + 1 in range(COLS) and grid[r][c + 1] == 0:
-                perimeter += 1
+            perimeter = 0
             for dr, dc in directions:
-                dfs(dr + r, dc + c)            
+                perimeter += dfs(dr + r, dc + c)
+            return perimeter
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    dfs(r, c)
-        return perimeter
+                    return dfs(r, c)
