21ed9baaf06c500c785372da7dc456048670523a
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-11 21:57:02 -0800

    Time: 735 ms (70.64%), Space: 18.3 MB (57.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
+        #intuition : we want to calculate the dp[amount] min count of coins to sum up to amount, we initialize each value of dp with amount + 1 as that will be the max value for each amount   
+        #we start from dp[0] = 0 => min number of coins it takes to sum upto 0 
+        dp = (amount + 1) * [amount + 1]
         dp[0] = 0
-        for a in range (1, amount + 1):
+        for a in range(1, amount + 1):
             for coin in coins:
+                #only proceed if amount - coin is greater than 0(eg. a = 7 coin = 4, we want to check first if 7-4 >= 0)
                 if a - coin >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - coin])
-        
-        return dp[amount] if dp[amount] != amount + 1 else -1
+                    #dp[7] = dp[7] {if it is already calculated} or 1 + dp[3] whichever is minimum
+                    dp[a] = min(dp[a], 1 + dp[a-coin])
+        return dp[amount] if dp[amount] != amount + 1 else - 1
