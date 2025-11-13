672775667ff787af45d61a08b5a99e195e825000
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-12 16:33:20 -0800

    Time: 311 ms (56.2%), Space: 18.1 MB (70.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-#store calculated values in cache by index and amount, make decisions by picking current index and by not picking to generate unique combinations
-        cache = {}
-        def dfs(i, a):
-            if a == amount:
-                return 1
-            #boundary condition for amount
-            if a > amount:
-                return 0
-            #boundary condition for index
-            if i == len(coins):
-                return 0
-            if (i, a) in cache:
-                return cache[(i, a)]
-            #either we pick the same coin and stay on the same index or we pick the next index for the same amount
-            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
-            return cache[(i, a)]
-        return dfs(0, 0)
+        #intuition : We use dynamic programming to count how many unique combinations of coins can make each amount up to the target.
+        #Each dp[a] accumulates ways by adding the number of ways to make a - coin for every coin.
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, - 1, - 1):
+            for a in range(1, amount + 1):
+                dp[a] += dp[a - coins[i]] if a >= coins[i] else 0
+        return dp[amount]
