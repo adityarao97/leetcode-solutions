0838609765cb6140befc31056c4254267e7fa837
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-12 16:39:58 -0800

    Time: 296 ms (58.19%), Space: 18.1 MB (59.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -4,7 +4,7 @@ class Solution:
         #Each dp[a] accumulates ways by adding the number of ways to make a - coin for every coin.
         dp = [0] * (amount + 1)
         dp[0] = 1
-        for i in range(len(coins) - 1, - 1, - 1):
+        for coin in coins:
             for a in range(1, amount + 1):
-                dp[a] += dp[a - coins[i]] if a >= coins[i] else 0
+                dp[a] += dp[a - coin] if a >= coin else 0
         return dp[amount]
