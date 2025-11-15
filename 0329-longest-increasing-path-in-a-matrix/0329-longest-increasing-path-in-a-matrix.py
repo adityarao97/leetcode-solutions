2f9f1cd4794099a7122588ef6c173144e8746637
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-14 16:50:47 -0800

    Time: 353 ms (5.02%), Space: 22.7 MB (22.65%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        #intuition : run a dfs on all cells find the maximum path where we have increasing cell values and store it in dp cache
         ROWS, COLS = len(matrix), len(matrix[0])
         dp = {}
-        visited = set()
         def dfs(r, c, prev):
             if r not in range(ROWS) or c not in range(COLS) or matrix[r][c] <= prev:
                 return 0
-            res = 0
             if (r, c) in dp:
-                return dp[(r, c)]
+                return dp[r, c]
+            res = 0
             for dr, dc in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
                 nr, nc = dr + r, dc + c
                 res = max(res, 1 + dfs(nr, nc, matrix[r][c]))
             dp[r, c] = res
-            return res 
+            return res
         maxLen = 0
         for r in range(ROWS):
             for c in range(COLS):
