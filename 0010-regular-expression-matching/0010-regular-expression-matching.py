42febb0503de17f911ee908a3d47db9bcb65f20f
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-16 13:29:17 -0800

    Time: 9699 ms (5%), Space: 18 MB (26.99%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,24 +1,21 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp = {}
+        #intuition : we compare each character in s (i index) with each character in p (j index) there's a match 
+        #if s[i] == p[j] or if p[j] == '.'
+        #if we encounter a '*' we have two choices either to match (recursive call with i+1, j) or not to match(recursive call with i, j+1)
         def dfs(i, j):
-            if (i, j) in dp:
-                return dp[(i, j)]
-            #i: index for s, j: index for p, if we exceed both the length of string we have matched successfully 
+            #if we exceed both s and p length we have matched successfully
             if i >= len(s) and j >= len(p):
                 return True
-            #if i hasn't exceeded length of s and we have j inbounds we haven't matched s completely so return False 
+            #if we exceeded p before s we didn't match s successfully
             if j >= len(p):
                 return False
-            #first we try to match either the characters at i and j or using a '.' and store it in match variable
-            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
-            #second we want to check for '*' we have two options choose '*' -> i+1, j(match) or don't choose '*' i, j+2(no match)
-            if (j + 1) < len(p) and p[j + 1] == '*':
-                dp[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j)) 
-                return dp[(i, j)]
+            #store match as a boolean for current indices
+            match = (i < len(s)) and (s[i] == p[j] or p[j] == '.')
+            #check for '*' case
+            if (j + 1 < len(p) and p[j + 1] == '*'):
+                return (match and dfs(i + 1, j)) or dfs(i, j + 2)
             if match:
-                dp[(i, j)] = res = dfs(i + 1, j + 1)
-                return dp[(i, j)]
-            dp[(i, j)] = False
-            return dp[(i, j)]
+                return dfs(i + 1, j + 1)
+            return False
         return dfs(0, 0)
