32ac1c6ca883850484f58d08e5389a03fa588901
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-16 14:17:05 -0800

    Time: 7 ms (79.64%), Space: 18.6 MB (74.77%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        #intuition : we keep l and r pointers which contains the max step we can reach from previous boundaries
+        #initialize l and r to 0, obtain the farthest you can go with each element between l and r until you reach the end
+        #calculate l = r + 1 and r = farthest for every boundary, total steps will be equal to each such calculation
         l = r = 0
-        minSteps = 0
-        while (r < len(nums) - 1):
+        steps = 0
+        while r < len(nums) - 1:
             farthest = 0
             for i in range(l, r + 1):
-                farthest = max(farthest, i + nums[i])
+                farthest = max(farthest, i + nums[i]) #you can reach from current value is i + nums[i]
             l = r + 1
             r = farthest
-            minSteps += 1
-        return minSteps
-                
+            steps += 1
+        return steps
