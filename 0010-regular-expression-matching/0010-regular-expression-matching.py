8a1c73f52354532689558e7eca89a8811670ca56
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-16 13:09:28 -0800

    Time: 9790 ms (5%), Space: 18.1 MB (26.99%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        def dfs(i, j):
+            #i: index for s, j: index for p, if we exceed both the length of string we have matched successfully 
+            if i >= len(s) and j >= len(p):
+                return True
+            #if i hasn't exceeded length of s and we have j inbounds we haven't matched s completely so return False 
+            if j >= len(p):
+                return False
+            #first we try to match either the characters at i and j or using a '.' and store it in match variable
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            #second we want to check for '*' we have two options choose '*' -> i+1, j(match) or don't choose '*' i, j+2(no match)
+            if (j + 1) < len(p) and p[j + 1] == '*':
+                return dfs(i, j + 2) or (match and dfs(i + 1, j)) 
+            if match:
+                return dfs(i + 1, j + 1)
+            return False
+        return dfs(0, 0)
