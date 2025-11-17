e809d93daac047d7fb22ae183bd18eac752a5ce8
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-17 15:08:48 -0800

    Time: 12 ms (94.85%), Space: 23.4 MB (43.91%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -3,11 +3,11 @@ class Solution:
         if sum(cost) > sum(gas):
             return -1
         total = 0
-        start = 0
+        first = 0
         for i in range(len(gas)):
             total += gas[i] - cost[i]
             if total < 0:
                 total = 0
-                start = i + 1
-        return start
-              
+                first = i + 1
+        return first
+
