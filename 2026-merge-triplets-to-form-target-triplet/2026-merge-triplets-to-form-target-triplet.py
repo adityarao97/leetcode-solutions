b82259fe1c750e290dc14853559977f546e7cbd5
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-18 19:06:43 -0800

    Time: 31 ms (46.88%), Space: 55.2 MB (56.08%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -1,5 +1,6 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        #intuition : for any element within the triplet if it is lesser than the target value in the index we ignore that as we can't possibly create the target triplet with them, we check for the rest values if we encounter a single match we add it to the set, if the set has 3 values we have found our target triplet
         good = set()
         for t in triplets:
             if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
@@ -8,4 +9,3 @@ class Solution:
                 if v == target[i]:
                     good.add(i)
         return len(good) == 3
-            
