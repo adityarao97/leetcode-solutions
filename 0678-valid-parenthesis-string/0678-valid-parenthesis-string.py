51069b61ccf81b339708ee1ca87c902a68dd0bff
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-11-18 18:08:15 -0800

    Time: 8 ms (8.12%), Space: 18.8 MB (5.98%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        #keep a single counter variable 'open' to track the number of open paranthesis, on encountering an open paranthesis increment else decrement, call recursively by increasing index, if at any time open falls below 0 we can't recover from it and return 0, for wild card we try all possibilities
+        dp = {}
+        def dfs(i, open):
+            if (i, open) in dp:
+                return dp[(i, open)]
+            if open < 0:
+                return False
+            if i == len(s):
+                return open == 0
+            if s[i] == '(':
+                dp[(i, open)] = dfs(i + 1, open + 1)
+                return dp[(i, open)]
+            if s[i] == ')':
+                dp[(i, open)] = dfs(i + 1, open - 1) 
+                return dp[(i, open)] 
+            if s[i] == '*':
+                dp[(i, open)] = dfs(i + 1, open) or dfs(i + 1, open - 1) or dfs(i + 1, open + 1) 
+                return dp[(i, open)]
+        return dfs(0, 0)
