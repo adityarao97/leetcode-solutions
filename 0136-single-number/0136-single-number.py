00285bac8d2344f85794a238089b705a619c82f7
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-12-01 18:19:43 -0800

    Time: 0 ms (100%), Space: 19.5 MB (76.64%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        #we will use xor operation: 1^1 = 0 and 0^0 = 0 all the same numbers will be 0 and the single element will be left
         res = nums[0]
         for i in range(1, len(nums)):
             res = res ^ nums[i]
