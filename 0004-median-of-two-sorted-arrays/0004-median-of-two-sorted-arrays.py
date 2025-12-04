cfc54c204f2a02da691c690b72a7cf5bc2a9180d
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-12-03 18:02:36 -0800

    Time: 1 ms (63.18%), Space: 18 MB (58.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,9 +1,9 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
-        #we want to assign A with lesser elements and run binary search on it
+        #A will have less elements and we want to run binary search on it
+        A, B = nums1, nums2
         if len(B) < len(A):
             A, B = B, A
         l, r = 0, len(A) - 1
@@ -13,16 +13,16 @@ class Solution:
             #mid of B
             j = half - i - 2
             Aleft = A[i] if i >= 0 else float("-inf")
-            Aright = A[i + 1] if i + 1 < len(A) else float("inf")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("inf")
             Bleft = B[j] if j >= 0 else float("-inf")
-            Bright = B[j + 1] if j + 1 < len(B) else float("inf")
-            #partition is correct
+            Bright = B[j + 1] if (j + 1) < len(B) else float("inf")
+            #correct partition case
             if Aleft <= Bright and Bleft <= Aright:
                 #odd case
                 if total % 2 != 0:
                     return min(Aright, Bright)
                 #even case
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                return (max(Aleft, Bleft) + min(Aright, Bright))/2
             elif Aleft > Bright:
                 r = i - 1
             else:
