d2aff8147b7c13c57a7d99785cae8a12537060c4
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2025-12-10 15:35:44 -0800

    Time: 7 ms (15.39%), Space: 18.1 MB (73.62%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,17 +1,16 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        #create an adjacency list
+        #create a new adjacency list to store src and dest tickets in reverse alphabetical order
         adj = defaultdict(list)
-        #store src, dst in adjacaency list in reverse order, we'll later pop it and append it to result so we get results in incremented order
         for src, dst in sorted(tickets)[::-1]:
             adj[src].append(dst)
         res = []
         def dfs(src):
             while adj[src]:
-                #pop the last element and call dfs from it
+                #we get lexicographically first value first as it is appended to the end of the list
                 dst = adj[src].pop()
                 dfs(dst)
-            #add the src when we reach a failure from while block
+            #when we reach the end of all the neighbours we start appending the lexicographically least value to the list
             res.append(src)
         dfs('JFK')
         return res[::-1]
