55c9717eff2bc5162a8bf2d5fb30dda9fb8ec554
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-05 21:33:41 -0800

    Time: 56 ms (9.82%), Space: 25.6 MB (15.74%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,5 +1,7 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        #intuition: we try to pair the heaviest people with the lightest people so as to minimize the number of boats req.
+        #for this we first sort the array and keep a pointer at the end and one at the start 
         people.sort()
         res = 0
         l, r = 0, len(people) - 1
