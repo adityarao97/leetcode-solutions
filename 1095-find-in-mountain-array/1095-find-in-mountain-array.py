2fc6a80c065e309e53a84b78deaa23f8fadfabf8
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-12 15:56:30 -0800

    Time: 40 ms (47.63%), Space: 20.1 MB (8.07%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -10,12 +10,13 @@ class Solution:
     def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
         length = mountainArr.length()
         l, r = 1, length - 2
-        #find peak element using binary search
+        #binary search to find peak element
         while l <= r:
             m = (l + r) // 2
             left = mountainArr.get(m - 1)
             mid = mountainArr.get(m)
             right = mountainArr.get(m + 1)
+            #condition to find the peak element
             if left < mid > right:
                 break
             elif left < mid < right:
@@ -23,23 +24,23 @@ class Solution:
             else:
                 r = m - 1
         peak = m
-        l, r = 0, peak - 1
-        #search in first half of the array which is in increasing order
+        l, r = 0 , peak
+        #binary search in first half of the array which is in increasing order till peak
         while l <= r:
             m = (l + r) // 2
             mid = mountainArr.get(m)
-            if mid == target:
+            if target == mid:
                 return m
-            elif target < mid:
+            if target < mid:
                 r = m - 1
             else:
                 l = m + 1
-        #search in second half of the array which is in decreasing order
-        l, r = peak, length - 1
+        l, r = peak + 1, length - 1
+        #binary search in second hald of the array which is in decreasing order
         while l <= r:
             m = (l + r) // 2
             mid = mountainArr.get(m)
-            if mid == target:
+            if target == mid:
                 return m
             elif target < mid:
                 l = m + 1
