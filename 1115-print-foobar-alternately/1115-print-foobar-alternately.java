bb961921066f9fcf2fb908414e3eaf78075ac04f
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-19 16:15:40 -0800

    Time: 21 ms (61.19%), Space: 45.2 MB (95.26%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,54 +1,29 @@
 class FooBar {
     private int n;
-    volatile boolean fooPrinted, barPrinted;
-    Lock lock;
-    Condition condition1, condition2;
-
+    Semaphore foo, bar;
     public FooBar(int n) {
         this.n = n;
-        fooPrinted = true;
-        barPrinted = false;
-        lock = new ReentrantLock();
-        condition1 = lock.newCondition();
-        condition2 = lock.newCondition();
+        foo = new Semaphore(1);
+        bar = new Semaphore(0);
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
-        lock.lock();
-        try{
-            for (int i = 0; i < n; i++) {            
-                while(!fooPrinted){
-                    condition2.await();
-                }
-                // printFoo.run() outputs "foo". Do not change or remove this line.
-                printFoo.run();
-                barPrinted = true;
-                fooPrinted = false;
-                condition1.signal();
-            }
+        
+        for (int i = 0; i < n; i++) {
+            foo.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            bar.release();
         }
-        finally{
-            lock.unlock();
-        }
-
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-        lock.lock();
-        try{
-            for (int i = 0; i < n; i++) {
-                while(!barPrinted){
-                    condition1.await();
-                }
-                barPrinted = false;
-                fooPrinted = true;
-                // printBar.run() outputs "bar". Do not change or remove this line.
-                printBar.run();
-                condition2.signal();
-            }
-        }
-        finally{
-            lock.unlock();
+        
+        for (int i = 0; i < n; i++) {
+            bar.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            foo.release();
         }
     }
 }
