7fe3636555941a2495127a9666ead18cdc0831b4
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-22 22:43:05 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -1,23 +1,25 @@
 class ZeroEvenOdd {
     private int n;
-    private Semaphore zero, odd, even;
+    Semaphore zero, odd, even;
     
     public ZeroEvenOdd(int n) {
         this.n = n;
-        this.zero = new Semaphore(1);
-        this.odd = new Semaphore(0);
-        this.even = new Semaphore(0);
+        zero = new Semaphore(1);
+        odd = new Semaphore(0);
+        even = new Semaphore(0);
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             zero.acquire();
-            printNumber.accept(0);
-            if (i % 2 == 0)
+            printNumber.accept(i);
+            if((i+1)%2==0){
                 even.release();
-            else
+            }
+            else{
                 odd.release();
+            }
         }
     }
 
