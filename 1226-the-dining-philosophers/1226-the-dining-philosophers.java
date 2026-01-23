dd048ba79b0728a54175ac8f7492e4e6d32d5c68
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-22 22:54:43 -0800

    Time: 11 ms (54.27%), Space: 53.1 MB (83.2%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.java
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -1,33 +1,35 @@
 class DiningPhilosophers {
     Semaphore semaphore;
-    Lock[] locks = new ReentrantLock[5];
+    Lock[] locks;
+
     public DiningPhilosophers() {
         semaphore = new Semaphore(4);
+        locks = new ReentrantLock[5];
         Arrays.fill(locks, new ReentrantLock());
     }
 
     // call the run() method of any runnable to execute its code
     public void wantsToEat(int philosopher,
-                           Runnable pickLeftFork,
-                           Runnable pickRightFork,
-                           Runnable eat,
-                           Runnable putLeftFork,
-                           Runnable putRightFork) throws InterruptedException {
-        
-        semaphore.acquire();
-        int left = philosopher;
-        int right = (philosopher + 1) % 5;
-        Lock leftLock = locks[left];
-        Lock rightLock = locks[right];
-        leftLock.lock();
-        rightLock.lock();
-        pickLeftFork.run();
-        pickRightFork.run();
-        eat.run();
-        putLeftFork.run();
-        leftLock.unlock();
-        putRightFork.run();
-        rightLock.unlock();
-        semaphore.release();
+            Runnable pickLeftFork,
+            Runnable pickRightFork,
+            Runnable eat,
+            Runnable putLeftFork,
+            Runnable putRightFork) throws InterruptedException {
+
+                semaphore.acquire();
+                int left = philosopher;
+                int right = (philosopher + 1) % 5;
+                Lock leftLock = locks[left];
+                Lock rightLock = locks[right];
+                leftLock.lock();
+                rightLock.lock();
+                pickLeftFork.run();
+                pickRightFork.run();
+                eat.run();
+                putLeftFork.run();
+                putRightFork.run();
+                leftLock.unlock();
+                rightLock.unlock();
+                semaphore.release();
     }
 }
