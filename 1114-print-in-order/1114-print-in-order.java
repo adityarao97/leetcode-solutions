4d0b630af6702f2d7e49134d7c1ae384c855ba1c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-22 22:04:38 -0800

    Time: 11 ms (66.14%), Space: 44.3 MB (11.24%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,56 +1,30 @@
 class Foo {
-    private volatile boolean firstDone, secondDone;
-    private Lock lock;
-    private Condition condition1, condition2; 
 
+    Semaphore semaphoreFirst, semaphoreSecond, semaphoreThird;
     public Foo() {
-        firstDone = secondDone = false;
-        lock = new ReentrantLock();
-        condition1 = lock.newCondition();
-        condition2 = lock.newCondition(); 
+        semaphoreFirst = new Semaphore(1);
+        semaphoreSecond = new Semaphore(0);
+        semaphoreThird = new Semaphore(0);
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        lock.lock();
-        try{
-            // printFirst.run() outputs "first". Do not change or remove this line.
-            printFirst.run();
-            firstDone = true;
-            condition1.signal();
-        }
-        finally{
-            lock.unlock();
-        }
+        semaphoreFirst.acquire();
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        semaphoreSecond.release();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        lock.lock();
-        try{
-            while(!firstDone){
-                condition1.await();
-            }
-            // printSecond.run() outputs "second". Do not change or remove this line.
-            printSecond.run();
-            secondDone = true;
-            condition2.signal();
-        }
-        finally{
-            lock.unlock();
-        }
-
+        semaphoreSecond.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        semaphoreThird.release();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        lock.lock();
-        try{
-            while(!secondDone){
-                condition2.await();
-            }
-            // printThird.run() outputs "third". Do not change or remove this line.
-            printThird.run();
-        }
-        finally{
-            lock.unlock();
-        }
+        semaphoreThird.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        semaphoreFirst.release();
     }
 }
