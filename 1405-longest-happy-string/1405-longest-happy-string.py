5ad68ddeb3a7fe00b7eb40bb94fabff3e36b31e7
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-30 10:14:01 -0800

    Time: 0 ms (100%), Space: 19.6 MB (7.29%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        res, maxHeap = "", []
-        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
-            if count!=0:
-                heapq.heappush(maxHeap, (count, char))
+        maxHeap, res = [], ""
+        for count, char in [(a, "a"), (b, "b"), (c, "c")]:
+            if count != 0:
+                heapq.heappush_max(maxHeap, (count, char))
         while maxHeap:
-            count, char = heapq.heappop(maxHeap)
+            count, char = heapq.heappop_max(maxHeap)
             if len(res) > 1 and res[-1] == res[-2] == char:
                 if not maxHeap:
                     break
-                count2, char2 = heapq.heappop(maxHeap)
+                count2, char2 = heapq.heappop_max(maxHeap)
                 res += char2
-                count2 += 1
+                count2 -= 1
                 if count2:
-                    heapq.heappush(maxHeap, (count2, char2))
+                    heapq.heappush_max(maxHeap, (count2, char2))
             else:
                 res += char
-                count += 1
+                count -= 1
             if count:
-                heapq.heappush(maxHeap, (count, char))
+                heapq.heappush_max(maxHeap, (count, char))
         return res
