75fa251b21a4e9a3077fe976dbd1e72c4f37a44a
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-01-30 10:14:35 -0800

    Time: 0 ms (100%), Space: 19.6 MB (7.29%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -3,7 +3,7 @@ class Solution:
         maxHeap, res = [], ""
         for count, char in [(a, "a"), (b, "b"), (c, "c")]:
             if count != 0:
-                heapq.heappush_max(maxHeap, (count, char))
+                heapq.heappush_max(maxHeap, [count, char])
         while maxHeap:
             count, char = heapq.heappop_max(maxHeap)
             if len(res) > 1 and res[-1] == res[-2] == char:
@@ -13,10 +13,10 @@ class Solution:
                 res += char2
                 count2 -= 1
                 if count2:
-                    heapq.heappush_max(maxHeap, (count2, char2))
+                    heapq.heappush_max(maxHeap, [count2, char2])
             else:
                 res += char
                 count -= 1
             if count:
-                heapq.heappush_max(maxHeap, (count, char))
+                heapq.heappush_max(maxHeap, [count, char])
         return res
