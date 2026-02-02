b8b3f432b4cfbec0568b16721e376d31285264bb
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-02-01 19:44:42 -0800

    Time: 363 ms (44.57%), Space: 45 MB (47.57%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,9 +1,9 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        maxProfit = [] #maxHeap stores profits which we can afford
-        minCapital = [(c, p) for c, p in zip(capital, profits)]
-        heapq.heapify(minCapital) #minHeap to store capital and corresponding profit
-        for i in range(k):
+        maxProfit = [] # maxheap to store profit which can be made
+        minCapital = [(c, p) for (c, p) in zip(capital, profits)] # minheap(c, p) to store min capital required to make that profit
+        heapq.heapify(minCapital)
+        for _ in range(k):
             while minCapital and minCapital[0][0] <= w:
                 c, p = heapq.heappop(minCapital)
                 heapq.heappush_max(maxProfit, p)
