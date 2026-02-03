018cd108a8d015036cf1af52b6939cd1a6032c0c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-02-03 13:18:45 -0800

    Time: 11 ms (70.54%), Space: 19.6 MB (46.18%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,26 +1,25 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()
-        posDiag = set() # r + c
-        negDiag = set() # r - c
+        col = set() #unique columns where queen has been inserted
+        posDiag = set() #diagonals where queen has been inserted, determined by r - c
+        negDiag = set() #diagonals where queen has been inserted, determined by r + c
         res = []
-        board = [["."]*n for i in range(n)]
+        board = [["."] * n for i in range(n)]
         def backtrack(r):
-            if r==n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+            if r == n:
+                res.append(["".join(row) for row in board])
                 return
             for c in range(n):
                 if c in col or (r + c) in posDiag or (r - c) in negDiag:
                     continue
-                board[r][c] = "Q"
                 col.add(c)
                 posDiag.add(r + c)
                 negDiag.add(r - c)
+                board[r][c] = "Q"
                 backtrack(r + 1)
-                board[r][c] = "."
                 col.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
+                board[r][c] = "."
         backtrack(0)
         return res
