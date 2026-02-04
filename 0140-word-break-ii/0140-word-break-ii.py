52bb41368b98706bacfb200d86ad30107f07f04f
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-02-03 21:58:00 -0800

    Time: 0 ms (100%), Space: 19.3 MB (45.05%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        cur, res = [], []
+        wordDict = set(wordDict)
+        def backtrack(i):
+            if i == len(s):
+                res.append(" ".join(cur))
+                return
+            for j in range(i, len(s)):
+                w = s[i: j + 1]
+                if w in wordDict:
+                    cur.append(w)
+                    backtrack(j + 1)
+                    cur.pop()
+        backtrack(0)
+        return res
