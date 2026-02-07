50d1ab94bd5eeab24ff1b0175235e7c1d5614b9c
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-02-06 23:44:55 -0800

    Time: 0 ms (100%), Space: 19.6 MB (22.49%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -3,13 +3,16 @@ class Solution:
         res = []
         stack = []
         def backtrack(openN, closedN):
+            #base case - append to result if opencount == closedcount == n
             if openN == closedN == n:
                 res.append("".join(stack.copy()))
                 return
+            #create combinations where open braces can be added -> if and only if openCount is less than n
             if openN < n:
                 stack.append("(")
                 backtrack(openN + 1, closedN)
                 stack.pop()
+            #create combinations where closed braces can be added -> if and only if closedCount is less than openCount 
             if closedN < openN:
                 stack.append(")")
                 backtrack(openN, closedN + 1)
