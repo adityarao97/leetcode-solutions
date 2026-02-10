89b5627ec11d036d55d287d206acf8d588b38be6
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-02-09 16:24:46 -0800

    Time: 3691 ms (26.5%), Space: 19.6 MB (22.55%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -4,16 +4,15 @@ class Solution:
         if sumLength % 4 != 0:
             return False
         sideLength = sumLength // 4
-        matchsticks.sort(reverse = True)
         sides = [0] * 4
+        matchsticks.sort(reverse = True)
         def dfs(i):
             if i == len(matchsticks):
                 return True
             for j in range(4):
                 if sides[j] + matchsticks[i] <= sideLength:
                     sides[j] += matchsticks[i]
-                    if dfs(i + 1):
-                        return True
+                    if dfs(i + 1): return True
                     sides[j] -= matchsticks[i]
             return False
         return dfs(0)
