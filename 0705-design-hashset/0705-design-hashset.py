0514df7128d629c7ec92f1845547055c56ff5106
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-03-01 15:16:56 -0800

    Time: 77 ms (28.52%), Space: 25.5 MB (37.67%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -5,12 +5,15 @@ class ListNode:
 
 class MyHashSet:
 
+    def hash(self, key):
+        return key % len(self.mySet)
+
     def __init__(self):
-        self.set = [ListNode(0) for _ in range(10**4)]
+        self.mySet = [ListNode(-1) for _ in range(10**4)]
 
     def add(self, key: int) -> None:
-        index = key % len(self.set)
-        cur = self.set[index]
+        index = self.hash(key)
+        cur = self.mySet[index]
         while cur.next:
             if cur.next.key == key:
                 return
@@ -18,8 +21,8 @@ class MyHashSet:
         cur.next = ListNode(key)
 
     def remove(self, key: int) -> None:
-        index = key % len(self.set)
-        cur = self.set[index]
+        index = self.hash(key)
+        cur = self.mySet[index]
         while cur.next:
             if cur.next.key == key:
                 cur.next = cur.next.next
@@ -27,8 +30,8 @@ class MyHashSet:
             cur = cur.next
 
     def contains(self, key: int) -> bool:
-        index = key % len(self.set)
-        cur = self.set[index]
+        index = self.hash(key)
+        cur = self.mySet[index]
         while cur.next:
             if cur.next.key == key:
                 return True
