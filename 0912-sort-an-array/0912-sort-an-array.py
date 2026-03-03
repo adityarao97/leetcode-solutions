c24f51ba9dfdc3cddbfea8bb1c593c7ae63d14be
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-03-02 17:48:07 -0800

    Time: 763 ms (25.72%), Space: 26.4 MB (81.76%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -4,7 +4,7 @@ class Solution:
             left, right = arr[L:M+1], arr[M+1:R+1]
             i, j, k = L, 0, 0
             while j < len(left) and k < len(right):
-                if left[j] <= right[k]:
+                if left[j] < right[k]:
                     arr[i] = left[j]
                     j += 1
                 else:
@@ -13,20 +13,20 @@ class Solution:
                 i += 1
             while j < len(left):
                 arr[i] = left[j]
-                j += 1
                 i += 1
+                j += 1
             while k < len(right):
                 arr[i] = right[k]
-                k += 1
                 i += 1
+                k += 1
 
-        def mergesort(arr, l, r):
+        def mergeSort(arr, l, r):
             if l >= r:
                 return
-            m = (l + r)//2
-            mergesort(arr, l, m)
-            mergesort(arr, m + 1, r)
+            m = (l + r) // 2
+            mergeSort(arr, l, m)
+            mergeSort(arr, m + 1, r)
             merge(arr, l, m, r)
 
-        mergesort(nums, 0, len(nums) - 1)
-        return nums
+        mergeSort(nums, 0, len(nums) - 1)
+        return nums 
