294608172e2875d8513df627f56256ffaef7f2dd
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-03-03 17:35:00 -0800

    Time: 39 ms (17.83%), Space: 27.7 MB (23.62%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [0] * len(nums)
-        prod = 1
-        zero_count = 0
+        #we create a prefix and suffix arrays to store all multiplication values of array except the current value
+        #output array will be multiplication of the current indexes prefix and suffix value respectively
+        prefix = [0] * len(nums)
+        suffix = [0] * len(nums)
+        output = [1] * len(nums)
+        prefix[0] = suffix[len(nums) - 1] = 1
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
         for i in range(len(nums)):
-            if nums[i] != 0:
-                prod *= nums[i]
-            else:
-                zero_count += 1
-                if zero_count > 1:
-                    return output
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                output = [0] * len(nums)
-                output[i] = prod
-                return output
-            else:
-                output[i] = prod // nums[i]
+            output[i] = prefix[i] * suffix[i]
         return output
