85b74fd03b4aa0cd6384df28d3d1f620876e73b2
Aditya Rao <32578979+adityarao97@users.noreply.github.com>
2026-07-29 17:50:28 -0700

    Time: 0 ms (100%), Space: 19.3 MB (24.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dict_br = { 
-            '}': '{', 
-            ']': '[', 
-            ')': '('
-        }
-        l_set = {'.', '}', ']', ')'}
-
-        l = -1
-
-        for r in range(0, len(s), 1):
-            if s[r] in dict_br:
-                if l != -1 and dict_br[s[r]] == s[l]:
-                    s = s[:l] + "." + s[l+1:]
-                    while l >= 0 and s[l] in l_set:
-                        l -= 1
+        bracketMap = { '(': ')', '[': ']', '{': '}' }
+        stack = []
+        for ch in s:
+            if ch in bracketMap:
+                stack.append(ch)
+            else:
+                if stack and ch == bracketMap[stack[-1]]:
+                    stack.pop()
                 else:
                     return False
-            else:
-                l = r
-
-        return True if l == -1 else False
+        return True if not stack else False 
